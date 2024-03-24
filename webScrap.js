f6f2a4bdf6b4e9730a2eef1fc273044d763f91e4
EsoraKyugo <tom.7trouillet@gmail.com>
2024-03-24 23:04:36 +0100

    scrap logique (deck:carte//)

--- webScrap.js
+++ webScrap.js
@@ -1,28 +1,49 @@
 import fetch from "node-fetch";
-import Cheerio from "cheerio";
+import cheerio from "cheerio";
 
-async function fetchData(url){
+async function fetchData(url) {
     try {
         const response = await fetch(url);
         const data = await response.text();
-        getName(data);
+        const deckNamesAndLinks = getNameAndLinks(data);
+        // Récupération des cartes pour chaque deck
+        for (const { name, link } of deckNamesAndLinks) {
+            const cardsData = await fetchCardData("https://deckbox.org" + link);
+            console.log("Nom du deck:", name);
+            getCard(cardsData);
+        }
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error);
     }
 }
 
-fetchData("https://deckbox.org/decks/mtg?f=");
+async function fetchCardData(url) {
+    try {
+        const response = await fetch(url);
+        return await response.text();
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données des cartes:', error);
+        return null;
+    }
+}
 
-function getName(html){
-    const $ = Cheerio.load(html);
-    //recup les info voulue
-    $("a.simple.left").each(function(){
-        // Sélectionne le texte de la balise <span>
-        const name = $(this).find("span").text().trim(); 
-        // Sélectionne l'attribut href de la balise <a>
-        const link = $(this).attr("href").trim(); 
-        console.log("Nom du deck:", name);
-        console.log("Lien vers le deck:", link);
+function getNameAndLinks(html) {
+    const $ = cheerio.load(html);
+    const deckNamesAndLinks = [];
+    $("a.simple.left").each(function () {
+        const name = $(this).find("span").text().trim();
+        const link = $(this).attr("href").trim();
+        deckNamesAndLinks.push({ name, link });
+    });
+    return deckNamesAndLinks;
+}
+
+function getCard(html) {
+    const $ = cheerio.load(html);
+    $("td.card_name").each(function () {
+        const cardName = $(this).find("a").text().trim();
+        console.log(cardName);
     });
 }
 
+fetchData("https://deckbox.org/decks/mtg?f=");
