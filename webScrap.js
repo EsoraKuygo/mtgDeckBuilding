1192bf408efc8db74d6cfbf02a13636f5e6fb0ba
EsoraKyugo <tom.7trouillet@gmail.com>
2024-03-24 23:13:00 +0100

    scap + encriture json

--- webScrap.js
+++ webScrap.js
@@ -1,17 +1,20 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
+import fs from 'fs/promises';
 
 async function fetchData(url) {
     try {
         const response = await fetch(url);
         const data = await response.text();
         const deckNamesAndLinks = getNameAndLinks(data);
+        const deckData = []; // Tableau pour stocker les données des decks
         // Récupération des cartes pour chaque deck
         for (const { name, link } of deckNamesAndLinks) {
             const cardsData = await fetchCardData("https://deckbox.org" + link);
-            console.log("Nom du deck:", name);
-            getCard(cardsData);
+            const cards = getCard(cardsData);
+            deckData.push({ name, cards }); // Ajouter les données du deck dans le tableau
         }
+        await fs.writeFile('data.json', JSON.stringify(deckData, null, 2)); // Écrire les données dans le fichier
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error);
     }
@@ -40,10 +43,12 @@ function getNameAndLinks(html) {
 
 function getCard(html) {
     const $ = cheerio.load(html);
+    const cards = [];
     $("td.card_name").each(function () {
         const cardName = $(this).find("a").text().trim();
-        console.log(cardName);
+        cards.push(cardName);
     });
+    return cards;
 }
 
 fetchData("https://deckbox.org/decks/mtg?f=");
