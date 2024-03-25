ad6c6cc71c6f90280092872ecd5f052ac7a95ae3
EsoraKyugo <tom.7trouillet@gmail.com>
2024-03-25 22:49:05 +0100

    add position + bug fix inventaire

--- inventory.js
+++ inventory.js
@@ -13,9 +13,10 @@ function fetchInventoryData() {
             // Afficher les données sur la page
             data.forEach(item => {
                 // Créer des éléments HTML pour chaque élément de l'inventaire
-                const DC = document.createElement('div')
-                const desc = document.createElement('p')
-                desc.textContent = `${item.name}` 
+                const DC = document.createElement('div');
+                DC.classList.add('card-container'); // Ajout de la classe card-container
+                const desc = document.createElement('p');
+                desc.textContent = item.name; 
                 desc.style.textAlign = 'center'; 
                 desc.style.marginBottom = '10px';
                 const cardImage = document.createElement('img');
@@ -26,13 +27,14 @@ function fetchInventoryData() {
                 cardImage.style.width = '265px';
                 DC.appendChild(desc);
                 DC.appendChild(cardImage);
-                block.appendChild(DC);
+                block.appendChild(DC); // Utilisation de DisplayInv
             });
         })
         .catch(error => {
             console.error('Erreur lors de la récupération des données de l\'inventaire:', error);
         });
 }
+
 
 // Appeler la fonction pour récupérer et afficher les données de l'inventaire
 fetchInventoryData();
